a28e649576eda82da95f48adb79c8d85da7d2ecd
Maltesius <andersmalta@gmail.com>
2025-12-08 16:35:29 +0100

    Added db functionality

--- Consumer/PostgresDB.cs
+++ Consumer/PostgresDB.cs
@@ -1,16 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Npgsql;
 
 namespace Consumer
 {
-    internal class PostgresDB : IDatabase
+    public class PostgresDB : IDatabase
     {
+        private static string Host = "localhost";
+        private static string User = "postgres";
+        private static string DBName = "postgres";
+        private static string Password = "test";
+        private static string Port = "5432";
+
+        private static string connString = $"Server={Host};Username={User};Database={DBName};Port={Port};Password={Password};SSLMode=Prefer";
+
+
         public void addRowToDB(int count, DateTime timeStamp)
         {
-            throw new NotImplementedException();
+            using (var conn = new NpgsqlConnection(connString)) 
+            {
+                Console.Out.WriteLine("Opening connection");
+                conn.Open();
+                
+
+                using (NpgsqlCommand cmd = new("CREATE TABLE IF NOT EXISTS messages(message_id SERIAL PRIMARY KEY, count INTEGER, timestamp TIMESTAMP)", conn))
+                {
+                    cmd.ExecuteNonQuery();
+                    Console.Out.WriteLine("Finished creating table");
+                }
+
+                using (NpgsqlCommand cmd = new("INSERT INTO messages (count, timestamp) VALUES (@c1, @t1)",conn))
+                {
+                    cmd.Parameters.AddWithValue("c1", count);
+                    cmd.Parameters.AddWithValue("t1", timeStamp);
+
+                    int nRows = cmd.ExecuteNonQuery();
+                    Console.Out.WriteLine($"Number of rows inserted={nRows}");
+                }
+
+                conn.CloseAsync().Wait();
+
+            }
+
+
+
         }
 
         public void connectToDB()
