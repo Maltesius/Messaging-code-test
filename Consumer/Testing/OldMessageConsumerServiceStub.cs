57607c4a58790841e3ce2edb19f6d88fbee66a35
Maltesius <andersmalta@gmail.com>
2025-12-09 19:07:51 +0100

    New stub for old messages

---
+++ Consumer/Testing/OldMessageConsumerServiceStub.cs
@@ -0,0 +1,18 @@
+﻿using Consumer.Interfaces;
+using Consumer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consumer.Testing
+{
+    public class OldMessageConsumerServiceStub : IConsumer
+    {
+        public (int, DateTime)? ConsumeMessage()
+        {
+            return (1, DateTime.UtcNow.Subtract(TimeSpan.FromMinutes(2)));
+        }
+    }
+}
