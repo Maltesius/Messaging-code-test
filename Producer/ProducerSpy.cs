62dac58f2ce8572e4ccd3b45d56c3673976bd05f
Maltesius <andersmalta@gmail.com>
2025-12-09 19:09:19 +0100

    Added new spy for producing messages

---
+++ Producer/ProducerSpy.cs
@@ -0,0 +1,19 @@
+﻿using Producer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    public class ProducerSpy : IProducer
+    {
+        public bool hasProduced = false;
+        public Task Produce()
+        {
+            hasProduced = true;
+            return Task.CompletedTask;
+        }
+    }
+}
