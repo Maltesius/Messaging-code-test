48739c04b1c7bbf704b680565ca639bacd3b55f0
Maltesius <andersmalta@gmail.com>
2025-12-09 19:08:11 +0100

    Handling count now

--- Consumer/Testing/PublisherSpy.cs
+++ Consumer/Testing/PublisherSpy.cs
@@ -10,10 +10,12 @@ namespace Consumer.Testing
     public class PublisherSpy : IPublisher
     {
         public bool hasPublished = false;
+        public int countToPublish;
 
         public void Publish(int count)
         {
             hasPublished = true;
+            countToPublish = count;
         }
     }
 }
